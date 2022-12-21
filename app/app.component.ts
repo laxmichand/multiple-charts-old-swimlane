fca793e4154c45ac887c594cf1204828f1cd3621
laxmichand <lcdhuvare30@gmail.com>
2022-12-21 15:27:55 +0530

    4 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { single, singleBar, multiBar } from './data';
+import { single, singleBar, multiBar, multiLine } from './data';
 
 @Component({
   selector: 'my-app',
@@ -30,6 +30,7 @@ export class AppComponent {
     this.getCalculate();
     Object.assign(this, { single });
     Object.assign(this, { singleBar });
+    Object.assign(this, { multiLine });
   }
 
   onSelect(data): void {
@@ -86,5 +87,37 @@ export class AppComponent {
 
   onSelectBar(event) {
     console.log(event);
+  }
+
+  //
+  multiLine: any[];
+  viewLine: any[] = [600, 300];
+
+  // options
+  legendLine: boolean = false;
+  showLabelsLine: boolean = true;
+  animationsLine: boolean = true;
+  xAxisLine: boolean = true;
+  yAxisLine: boolean = true;
+  showYAxisLabelLine: boolean = true;
+  showXAxisLabelLine: boolean = true;
+  xAxisLabelLine: string = 'Year';
+  yAxisLabelLine: string = 'Population';
+  timelineLine: boolean = true;
+
+  colorSchemeLine = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5'],
+  };
+
+  onSelectLine(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivateLine(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivateLine(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 }
